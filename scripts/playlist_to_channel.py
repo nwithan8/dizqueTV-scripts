c0d8e2ca0d41da9f5bd4da54394d86ad6aa56c14
Nate <nathan.harris@force5solutions.com>
2020-10-02 23:17:32 -0400

    Temp workaround for making a new channel. Proper fix in future API library version

--- scripts/playlist_to_channel.py
+++ scripts/playlist_to_channel.py
@@ -16,8 +16,14 @@ PLEX_URL = "http://localhost:32400"
 PLEX_TOKEN = "thisisaplextoken"
 
 parser = argparse.ArgumentParser()
-parser.add_argument('playlist_name', type=str, help="Name of Plex playlist to convert to a channel")
-parser.add_argument('channel_number', type=int, help="DizqueTV channel to add playlist to.")
+parser.add_argument('playlist_name',
+                    type=str,
+                    help="Name of Plex playlist to convert to a channel")
+parser.add_argument('-c', '--channel_number',
+                    nargs='?',
+                    type=int,
+                    default=None,
+                    help="DizqueTV channel to add playlist to.")
 args = parser.parse_args()
 
 
@@ -38,20 +44,38 @@ class Plex:
 
 
 dtv = API(url=DIZQUETV_URL)
-plex = Plex(url=PLEX_URL, token=PLEX_TOKEN)
+plex = Plex(url=PLEX_URL,
+            token=PLEX_TOKEN)
 plex_playlist = plex.get_playlist(playlist_name=args.playlist_name)
-channel = dtv.get_channel(channel_number=args.channel_number)
-to_add = []
-if channel:
-    if plex_playlist:
-        for item in plex_playlist.items():
-            item = dtv.convert_plex_item_to_program(plex_item=item, plex_server=plex.server)
-            if item:
-                print(f"Adding {item.title}...")
-                to_add.append(item)
-        if channel.delete_all_programs():
-            dtv.add_programs_to_channels(programs=to_add, channels=[channel])
-    else:
-        print(f"Could not find {args.playlist_name} playlist.")
+first_needed = False
+if args.channel_number:
+    channel = dtv.get_channel(channel_number=args.channel_number)
+    if not channel:
+        print(f"Could not find channel #{args.channel_number}")
+        exit(1)
 else:
-    print(f"Could not find channel #{args.channel_number}")
+    first_needed = True
+    channel_numbers = dtv.channel_numbers
+    if channel_numbers:
+        new_channel_number = max(channel_numbers) + 1
+    else:
+        new_channel_number = 1
+to_add = []
+if plex_playlist:
+    for item in plex_playlist.items():
+        item = dtv.convert_plex_item_to_program(plex_item=item,
+                                                plex_server=plex.server)
+        if item:
+            if first_needed:
+                print(f"Creating '{plex_playlist.title}' channel on dizqueTV...")
+                channel = dtv.add_channel(programs=[item],
+                                          name=plex_playlist.title,
+                                          number=new_channel_number)
+                first_needed = False
+            print(f"Adding {item.title}...")
+            to_add.append(item)
+    if channel.delete_all_programs():
+        dtv.add_programs_to_channels(programs=to_add,
+                                     channels=[channel])
+else:
+    print(f"Could not find {args.playlist_name} playlist.")
