2362b10e0f573ecdf617c84e6bc2d982cbf681bf
Nate <nathan.harris@force5solutions.com>
2020-10-23 18:30:01 -0400

    New args, because I can't help myself over-engineering shit

--- scripts/make_blank_channels.py
+++ scripts/make_blank_channels.py
@@ -10,25 +10,41 @@ from plexapi import server, playlist
 from dizqueTV import API
 
 # COMPLETE THESE SETTINGS
-DIZQUETV_URL = "http://localhost:8000"
+DIZQUETV_URL = "http://192.168.1.27:8000"
 
 parser = argparse.ArgumentParser()
-parser.add_argument('start_number',
+parser.add_argument('numbers',
+                    nargs="+",
                     type=int,
-                    help="Channel number to start at.")
-parser.add_argument('end_number',
+                    help="Channel number to start at, or a list of individual channel numbers.")
+parser.add_argument('-t',
+                    '--thru',
                     type=int,
+                    default=None,
                     help="Channel number to end at.")
 args = parser.parse_args()
 
-
 dtv = API(url=DIZQUETV_URL)
 
-for number in range(args.start_number, args.end_number + 1):
-    channel = dtv.add_channel(programs=[],
-                              number=number,
-                              handle_errors=True)
-    if channel:
-        print(f"Created channel {number} on dizqueTV.")
+channel_numbers = []
+if args.thru:
+    if len(args.numbers) > 1:
+        print("Please provide only one start number if you are using --thru.")
+        exit(1)
     else:
-        print(f"Could not create channel {number} on dizqueTV.")
+        for num in range(args.numbers[0], args.thru + 1):
+            channel_numbers.append(num)
+else:
+    channel_numbers = args.numbers
+
+for number in channel_numbers:
+    if number in dtv.channel_numbers:
+        print(f"Channel {number} already exists. Not going to overwrite it.")
+    else:
+        channel = dtv.add_channel(programs=[],
+                                  number=number,
+                                  handle_errors=True)
+        if channel:
+            print(f"Created {channel.name} on dizqueTV.")
+        else:
+            print(f"Could not create channel {number} on dizqueTV.")
