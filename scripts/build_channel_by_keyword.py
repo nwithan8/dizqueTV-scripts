cf6f3fa880c8268e7cbeae7b6abf63735ff9ec0e
Nate Harris <n8gr8gbln@gmail.com>
2021-11-20 20:37:47 -0700

    - Allow for multiple keywords when using build_channel_by_keyword.py - Allow for updating existing channel via channel number in build_channel_by_keyword.py

--- scripts/build_channel_by_keyword.py
+++ scripts/build_channel_by_keyword.py
@@ -21,9 +21,10 @@ PLEX_TOKEN = "thisisaplextoken"
 # DO NOT EDIT BELOW THIS LINE
 parser = argparse.ArgumentParser()
 parser.add_argument("keywords", nargs="*", type=str, help="Keyword to search for in Plex")
-parser.add_argument("-s", '--sections',  nargs="+", type=str, required=True, help="Plex media section(s) to use")
+parser.add_argument("-s", '--sections', nargs="+", type=str, required=True, help="Plex media section(s) to use")
 parser.add_argument("-N", '--channel_name', nargs="?", type=str, help="name of DizqueTV channel to create")
-parser.add_argument("-c", '--channel_number', nargs='?', type=int, default=None, help="DizqueTV channel to add playlist to.")
+parser.add_argument("-c", '--channel_number', nargs='?', type=int, default=None,
+                    help="DizqueTV channel to add playlist to.")
 parser.add_argument("-t", '--token', nargs='?', type=str, default=None, help="Override the script's plex token.")
 parser.add_argument("-x", "--shuffle", action="store_true", help="Shuffle items once channel is completed.")
 parser.add_argument("-v", "--verbose", action="store_true", help="Verbose (for debugging)")
@@ -63,18 +64,23 @@ for item in matching_items:
             if (hasattr(episode, "originallyAvailableAt") and episode.originallyAvailableAt) and \
                     (hasattr(episode, "duration") and episode.duration):
                 final_programs.append(episode)
-new_channel = dtv.add_channel(programs=final_programs,
+
+if new_channel_number in dtv.channel_numbers:
+    channel = dtv.get_channel(channel_number=new_channel_number)
+    channel.add_programs(programs=final_programs, plex_server=plex_server)
+else:
+    channel = dtv.add_channel(programs=final_programs,
                               plex_server=plex_server,
                               number=new_channel_number,
                               name=f"{channel_name}",
                               handle_errors=True)
-if not new_channel:
-    print("Failed to create channel.")
+
+if not channel:
+    print("Failed to update channel.")
     exit(1)
 
+print(f"Channel {new_channel_number} '{channel_name}' successfully updated.")
 
-print(f"Channel {new_channel_number} '{channel_name}' successfully created.")
 if args.shuffle:
     print("Shuffling channel items...")
-    new_channel.sort_programs_randomly()
-
+    channel.sort_programs_randomly()
